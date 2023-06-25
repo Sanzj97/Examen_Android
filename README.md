4a50a9ce20c316e477390a1f3c83b76154bc2d18
WJAS <137307354+Sanzj97@users.noreply.github.com>
2023-06-25 16:19:40 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Examen I Parcial Android
+Integrantes:
+- Winston Joel Aburto Sanchez
+
+<div align="center">
+  <img src="https://github.com/Sanzj97/Examen/blob/master/app/src/main/res/drawable/android.png?raw=true" alt="Icon" width="75%" />
+</div>
---
+++ app/src/main/java/com/example/examen/Data/Lista.kt
@@ -0,0 +1,20 @@
+package com.example.examen.Data
+
+class Lista {
+    val list_personas: List<Personas> = listOf(
+        Personas("Winston Joel", "Aburto Sanchez", "76163008"),
+        Personas("Brenda Guadalupe", "Gutierrez Vasquez", "84568975"),
+        Personas("Marcia Veronica", "Sanchez Jimenez", "84568975"),
+        Personas("Emilio Antonio", "Aburto", "84568975"),
+        Personas("Jenifer Veronica", "Garcia Sanchez", "84568975"),
+        Personas("Xochilt Margarita", "Aburto Sanchez", "845689756"),
+        Personas("Lesther Antonio", "Castro Aleman", "84568975"),
+        Personas("Brenda Urania", "Vasquez Aleman", "84568975"),
+        Personas("Justo Cesar", "Gutierrez", "845689756"),
+        Personas("Ronald ", "Carcia", "84568975"),
+        Personas("Ernesto Jose", "Ruiz", "84568975"),
+        Personas("Carlos Jose", "Ruiz Guardado", "84568975"),
+        Personas("Junior", "Ruiz Guardado", "84568975"),
+        Personas("Karla Partricia", "Aleman Mairena", "84568975")
+    )
+}
